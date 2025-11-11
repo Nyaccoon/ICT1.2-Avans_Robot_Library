31feb7eb81e314d61b834696098d7936141a3a6e
Marcy <nyaccoon@gmail.com>
2025-11-11 13:06:58 +0100

    added more button functionality
    added getstatebool() which returns the state as a boolean
    
    and added a getbuttonup() function which only returns true when state change goes from false to true

--- StatisticalRobot-Lib/Button.cs
+++ StatisticalRobot-Lib/Button.cs
@@ -6,6 +6,7 @@ public class Button
 {
     private readonly int _pin;
     private readonly bool _defHigh;
+    private bool _previousState;
 
     /// <summary>
     /// This is a digital device
@@ -19,9 +20,37 @@ public class Button
         _pin = pin;
         _defHigh = defHigh;
     }
-
+    /// <summary>
+    /// returns "Released" when button is up and "Pressed" when button is down
+    /// </summary>
+    /// <returns>string</returns>
     public string GetState()
     {
         return (Robot.ReadDigitalPin(_pin) == PinValue.High) ? "Released" : "Pressed";
+    }
+
+    /// <summary>
+    /// returns True when button is up and False when button is down
+    /// </summary>
+    /// <returns>boolean return type</returns>
+    public bool GetStateBool()
+    {
+        return (Robot.ReadDigitalPin(_pin) == PinValue.High);
+    }
+    /// <summary>
+    /// only returns true on a state change of down to up, must be called every cycle like with an if statement
+    /// </summary>
+    /// <returns>boolean return type</returns>
+    public bool GetButtonUp()
+    {
+        bool current = GetStateBool();
+        bool retVal = false;
+        if (!current && _previousState)
+        {
+            retVal = true;
+        }
+        
+        _previousState = current;
+        return retVal;
     }
 }
